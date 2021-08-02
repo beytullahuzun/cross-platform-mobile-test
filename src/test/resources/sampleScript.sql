5cdc86f3ad64e09e164a57d06cdc3d93f935a824
Beytullah Uzun <46282812+beytullahuzun@users.noreply.github.com>
2021-08-02 11:53:55 +0200

    Update sampleScript.sql

--- src/test/resources/sampleScript.sql
+++ src/test/resources/sampleScript.sql
@@ -7,9 +7,9 @@ CREATE TABLE exampleDB.accounts(
    Place_Of_Birth VARCHAR(255),
    Country VARCHAR(255)
 );
-insert into accounts values('Mobile', 'Samplea', DATE('1981-12-05'), 'London', 'UK');
-insert into accounts values('Mobile', 'Sampleb', DATE('1982-12-05'), 'London', 'UK');
-insert into accounts values('Mobile', 'Samplec', DATE('1983-12-05'), 'London', 'UK');
-insert into accounts values('Mobile', 'Sampled', DATE('1984-12-05'), 'London', 'UK');
-insert into accounts values('Mobile', 'Samplee', DATE('1985-12-05'), 'London', 'UK');
+insert into accounts values('Mobile', 'Samplea', DATE('1981-12-05'), 'Madrid', 'SP');
+insert into accounts values('Mobile', 'Sampleb', DATE('1982-12-05'), 'Madrid', 'SP');
+insert into accounts values('Mobile', 'Samplec', DATE('1983-12-05'), 'Madrid', 'SP');
+insert into accounts values('Mobile', 'Sampled', DATE('1984-12-05'), 'Madrid', 'SP');
+insert into accounts values('Mobile', 'Samplee', DATE('1985-12-05'), 'Madrid', 'SP');
 select * from mydatabase.accounts;
